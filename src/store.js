3ecf6ade7d64bc31505c79c825779e22d4f8021a
theophilus doku <theodoku89@gmail.com>
2023-04-08 17:09:40 +0000

    Added Modal.js and modalSlice.js, modified CartContainer.js, CartItems.js, App.js, store.js

--- src/store.js
+++ src/store.js
@@ -1,10 +1,12 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore } from '@reduxjs/toolkit';
 import cartReducer from './features/cartSlice';
+import modalReducer from './features/modalSlice';
 
  const store = configureStore({
-    reducer: {
-        cart: cartReducer,
-    },
- });
+  reducer: {
+    cart: cartReducer,
+    modal: modalReducer,
+  },
+});
 
 export default store;
